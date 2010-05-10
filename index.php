59a145da6cfd8bbcf95f0b1b7c2cb110b84608db
Arian <arian@aryweb.nl>
2010-05-10 18:05:23 +0200

    Fix troubles with html in the markdown

--- index.php
+++ index.php
@@ -39,7 +39,6 @@ $markdown = file_get_contents($filePath);
 // Get the content of the current page
 $md = new MarkdownExtra_Parser();
 $md->no_markup = true;
-$md->no_entities = true;
 $content = $md->transform($markdown);
 // Replace urls with the right ones
 $content = str_replace('href="/','href="'.$baseurl.'/',$content);
